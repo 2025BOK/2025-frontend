16eb366f7a766e68d21e7f2d72eac3142549a499
karamjoen <duddj1203@naver.com>
2025-04-08 00:29:23 +0900

    fix:index.js , delete node_modulesa

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import TestView from '../views/TestView.vue'
 import NotFoundPage from '../views/NotFoundPage.vue'
+import { component } from 'vue/types/umd';
 
 Vue.use(VueRouter);
 
